4a18bc9ac6668c299ab889790c59d69472e4512f
ZarinTasnimKhan <zarintm1221@gmail.com>
2021-10-21 14:10:07 +0800

    first commit

---
+++ src/oop/Main.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Main
+{
+    int x = 10;
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        System.out.println("value of x = "+main.x);
+    }
+}
